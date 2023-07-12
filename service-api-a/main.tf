74ce8238959672482e8be50ac51760d6b1905b4d
alessiolunghi <93533909+alessiolunghi@users.noreply.github.com>
2023-07-12 16:18:46 +0100

    Output file

--- service-api-a/main.tf
+++ service-api-a/main.tf
@@ -2,3 +2,11 @@ resource "random_password" "secret" {
   length  = 8
   special = true
 }
+
+output "file_content" {
+  value = yamldecode(file("/mnt/workspace/ecs-test-config.yaml"))
+}
+
+output "echo" {
+  value ="service-b"
+}
